954c28c6ddf2785486b6240d526dcc74bf5fea77
jishnasun <144394163+jishnasun@users.noreply.github.com>
2026-02-28 12:57:50 -0500

    Delete testBayesianModel.py
    ended up using lexemes.tsv instead of csvs

--- testBayesianModel.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-pathb = "COG403-SemanticShifting/cldf/"  # Change path to cldf directory
-values = pd.read_csv(pathb + "ValueTable.csv")
-forms = pd.read_csv(pathb + "FormTable.csv")
-languages = pd.read_csv(pathb + "LanguageTable.csv")

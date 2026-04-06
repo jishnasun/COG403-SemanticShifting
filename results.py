94ff4fb2efe5fb01d4785843a5a8e06bfbf72d40
AliceBaker73 <123594341+AliceBaker73@users.noreply.github.com>
2026-04-06 14:32:14 -0400

    added best/worst lang visualization code

--- results.py
+++ results.py
@@ -1,46 +1,153 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from test_file import run_eval
-from train_test import run_feval
+import seaborn as sns
+
+from test_file_mod import run_eval, run_eval_lang
+from train_test_mod import run_feval, run_feval_lang
+from singLangTestCase import get_best_global, get_worst, get_best_fam
+
+PTYPE = 0
+# TYPES:
+# 0 - overall bar plots
+# 1 - single lang plots
+
+PLOT = 1
+# TYPES:
+# 0 - don't generate plot
+# 1 - generate plot
 
 
-# get the results
-full_1, full_5 = run_eval('full')
-prior_1, prior_5 = run_eval('prior')
-sim_1, sim_5 = run_eval('similarity')
-fam_1, fam_5 = run_feval()
+################################################################################
+#################################### BAR PLOTS #################################
+################################################################################
+def plots(cols, data):
+    fulls = [data['full'][0], data['full'][1]]
+    priors = [data['prior'][0], data['prior'][1]]
+    sims = [data['sim'][0], data['sim'][1]]
+    fams = [data['fam'][0], data['fam'][1]]
+    cats = ['top-1', 'top-5']
 
-fulls = [full_1, full_5]
-priors = [prior_1, prior_5]
-sims = [sim_1, sim_5]
-fams = [fam_1, fam_5]
-cats = ['top-1', 'top-5']
+    w, x = 0.2, np.arange(len(cats))
+
+    fig, ax = plt.subplots()
+
+    b1 = ax.bar(x-0.3, fulls, width=w, label='P(T|S)')
+    b2 = ax.bar(x-0.1, sims, width=w, label='P(S|T)')
+    b3 = ax.bar(x+0.1, priors, width=w, label='P(T)')
+    b4 = ax.bar(x+0.3, fams, width=w, label='P(Tf|S)')
+
+    for bars in [b1, b2, b3, b4]:
+        ax.bar_label(bars, fmt='%.3f', padding=1)
+
+    ax.set_xticks(x)
+    plt.yticks(np.arange(0, 1.1, step=0.2))
+    ax.set_xticklabels(cats)
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Top-1 and Top-5 Accuracy Across Models')
+
+    if cols == 1:
+        for bar in b1:
+            bar.set_color('#1f497d')
+        for bar in b2:
+            bar.set_color('#4f81bd')
+        for bar in b3:
+            bar.set_color('#4bacc6')
+        for bar in b4:
+            bar.set_color('#cadeefff')
+
+    ax.legend(loc='upper center', ncol=2)
+
+    if cols == 1:
+        fig.savefig(f"plot_overall2.0_c.png")
+        plt.close(fig)
+        return
+
+    fig.savefig(f"plot_overall2.0.png")
+    plt.close(fig)
+    return
 
 
-# make graph
-w, x = 0.2, np.arange(len(cats))
+if PTYPE == 0:
+    results = {
+        'full': run_eval('full'),
+        'prior': run_eval('prior'),
+        'sim': run_eval('similarity'),
+        'fam': run_feval()
+    }
 
-fig, ax = plt.subplots()
-b1 = ax.bar(x-0.3, fulls, width=w, label='P(T|S)', color='#1f497d')
-b2 = ax.bar(x-0.1, sims, width=w, label='P(S|T)', color='#4f81bd')
-b3 = ax.bar(x+0.1, priors, width=w, label='P(T)', color='#4bacc6')
-b4 = ax.bar(x+0.3, fams, width=w, label='P(Tf|S)', color='#cadeefff')
+    if PLOT == 1:
+        plots(0, results)
+        plots(1, results)
 
-for bars in [b1, b2, b3, b4]:
-    ax.bar_label(bars, fmt='%.3f', padding=1)
 
-ax.set_xticks(x)
-ax.set_xticklabels(cats)
-ax.set_ylabel('Accuracy')
-ax.set_title('Top-1 and Top-5 Accuracy Across Models')
-ax.legend()
+################################################################################
+############################### SINGLE LANG PLOTS ##############################
+################################################################################
+def single_plot(cols, data, label):
+    print("LOCATION: single plot func")
+    lang = data['language']
 
-fig.savefig("plot1.png")
-plt.show()
+    fulls = [data['full'][0], data['full'][1]]
+    priors = [data['prior'][0], data['prior'][1]]
+    sims = [data['sim'][0], data['sim'][1]]
+    fams = [data['fam'][0], data['fam'][1]]
+    cats = ['top-1', 'top-5']
 
-# print(f'FULL - top1: {full_1}, top5: {full_5}')
-# print(f'PRIOR - top1: {prior_1}, top5: {prior_5}')
-# print(f'SIM - top1: {sim_1}, top5: {sim_5}')
-# print(f'FAM - top1: {fam_1}, top5: {fam_5}')
+    w, x = 0.2, np.arange(len(cats))
+
+    fig, ax = plt.subplots()
+
+    b1 = ax.bar(x-0.3, fulls, width=w, label='P(T|S)')
+    b2 = ax.bar(x-0.1, sims, width=w, label='P(S|T)')
+    b3 = ax.bar(x+0.1, priors, width=w, label='P(T)')
+    b4 = ax.bar(x+0.3, fams, width=w, label='P(Tf|S)')
+
+    for bars in [b1, b2, b3, b4]:
+        ax.bar_label(bars, fmt='%.3f', padding=1)
+
+    ax.set_xticks(x)
+    plt.yticks(np.arange(0, 1.1, step=0.2))
+    ax.set_xticklabels(cats)
+    ax.set_ylabel('Accuracy')
+    ax.set_title(f'{lang}: Top-1 and Top-5 Accuracy Across Models')
+
+    if cols == 1:
+        for bar in b1:
+            bar.set_color('#1f497d')
+        for bar in b2:
+            bar.set_color('#4f81bd')
+        for bar in b3:
+            bar.set_color('#4bacc6')
+        for bar in b4:
+            bar.set_color('#cadeefff')
+
+    ax.legend(loc='upper center', ncol=2)
+
+    if cols == 1:
+        fig.savefig(f"plot_{label}_c.png")
+        plt.close(fig)
+        return
+
+    fig.savefig(f"plot_{label}.png")
+    plt.close(fig)
+    return
+
+
+if PTYPE == 1:
+    results_best_full = get_best_global()
+    results_worst = get_worst()
+    results_best_fam = get_best_fam()
+
+    results = {
+        'best_full': results_best_full,
+        'best_fam': results_best_fam,
+        'worst': results_worst
+    }
+
+    if PLOT == 1:
+        for label, data in results.items():
+            single_plot(0, data, label)
+            single_plot(1, data, label)
+
 

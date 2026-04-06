8b750084c27d24bdb6a31b5eff2ca590987b9fc4
jishnasun <144394163+jishnasun@users.noreply.github.com>
2026-04-06 13:18:31 -0400

    Updated incorrect variable

--- train_test_2.0.py
+++ train_test_2.0.py
@@ -116,7 +116,7 @@ def evaluate_hierarchical_model(df, P_S_given_T, lang_to_family, lang_to_subgrou
             P_T = P_T_global
 
         P_T_given_S = P_S_given_T * P_T
-        np.fill_diagonal(P, 0)  # set all P(T|S) where T == S to 0
+        np.fill_diagonal(P_T_given_S, 0)  # set all P(T|S) where T == S to 0
 
         
         row_sums = P_T_given_S.sum(axis=1, keepdims=True)
